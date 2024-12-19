eeead311beb9f0d4cb303851b8e5ac5ec7ef33d8
Joak1ngglb <solojuegoschexd@gmail.com>
2024-12-19 14:41:37 -0300

    Stats test

--- Intersect.Server.Core/Entities/Player.cs
+++ Intersect.Server.Core/Entities/Player.cs
@@ -1133,6 +1133,16 @@ public partial class Player : Entity
             }
         }
 
+        // Add ArmorPenetration only to Health (vital == (int)Vital.Health)
+        if (vital == (int)Vital.Health)
+        {
+            classVital += Stat[(int)Enums.Stat.ArmorPenetration].Value();
+        }
+        if (vital == (int)Vital.Mana)
+        {
+            classVital += Stat[(int)Enums.Stat.ArmorPenetration].Value();
+        }
+
         var baseVital = classVital;
 
         // Loop through equipment and see if any items grant vital buffs
@@ -5929,13 +5939,21 @@ public partial class Player : Entity
     //Stats
     public void UpgradeStat(int statIndex)
     {
-        if (Stat[statIndex].BaseStat + StatPointAllocations[statIndex] < Options.MaxStatValue && StatPoints > 0)
+        if (statIndex == 5)
         {
-            StatPointAllocations[statIndex]++;
-            StatPoints--;
-            PacketSender.SendEntityStats(this);
-            PacketSender.SendPointsTo(this);
-            UnequipInvalidItems();
+            PacketSender.SendChatMsg(this, "Nie mozesz ulepszyc statystyki armorpenetration.", ChatMessageType.Error);
+        }
+        else
+        {
+            // Ulepszanie statystyki
+            if (Stat[statIndex].BaseStat + StatPointAllocations[statIndex] < Options.MaxStatValue && StatPoints > 0)
+            {
+                StatPointAllocations[statIndex]++;
+                StatPoints--;
+                PacketSender.SendEntityStats(this);
+                PacketSender.SendPointsTo(this);
+                UnequipInvalidItems();
+            }
         }
     }
 

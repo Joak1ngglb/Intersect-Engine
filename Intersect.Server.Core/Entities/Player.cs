f55c57f798e4a82ec5c76803fb3189e67d191e0b
Joak1ngglb <solojuegoschexd@gmail.com>
2024-12-20 02:40:07 -0300

    Exp Porcentual

--- Intersect.Server.Core/Entities/Player.cs
+++ Intersect.Server.Core/Entities/Player.cs
@@ -1314,7 +1314,10 @@ public partial class Player : Entity
                         var partyExperience = (int)(descriptor.Experience * multiplier) / partyMembersInXpRange.Length;
                         foreach (var partyMember in partyMembersInXpRange)
                         {
-                            partyMember.GiveExperience(partyExperience);
+                            //CustomChange
+                            partyMember.GiveExperience(
+                                ExpModifiedByLevel(descriptor.Level, partyExperience, partyMember.Level)
+                            );
                             partyMember.UpdateQuestKillTasks(entity);
                         }
 
@@ -1331,7 +1334,8 @@ public partial class Player : Entity
                     }
                     else
                     {
-                        GiveExperience(descriptor.Experience);
+                        //CustomChange
+                        GiveExperience(ExpModifiedByLevel(descriptor.Level, descriptor.Experience));
                         UpdateQuestKillTasks(entity);
                     }
 

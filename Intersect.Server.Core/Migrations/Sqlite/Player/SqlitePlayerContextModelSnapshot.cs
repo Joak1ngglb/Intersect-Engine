d4baca5ce6b19e3c24fac2abff82f43cbce54958
Robbie Lodico <pandacoder@pm.me>
2025-01-14 05:11:13 +0100

    feat: offline guild invites

--- Intersect.Server.Core/Migrations/Sqlite/Player/SqlitePlayerContextModelSnapshot.cs
+++ Intersect.Server.Core/Migrations/Sqlite/Player/SqlitePlayerContextModelSnapshot.cs
@@ -673,6 +673,12 @@ namespace Intersect.Server.Migrations.Sqlite.Player
                         .HasColumnType("TEXT")
                         .HasColumnName("NameColor");
 
+                    b.Property<Guid?>("PendingGuildInviteFromId")
+                        .HasColumnType("TEXT");
+
+                    b.Property<Guid?>("PendingGuildInviteToId")
+                        .HasColumnType("TEXT");
+
                     b.Property<Guid>("PersonalMapInstanceId")
                         .HasColumnType("TEXT");
 
@@ -732,6 +738,10 @@ namespace Intersect.Server.Migrations.Sqlite.Player
                     b.HasKey("Id");
 
                     b.HasIndex("GuildId");
+
+                    b.HasIndex("PendingGuildInviteFromId");
+
+                    b.HasIndex("PendingGuildInviteToId");
 
                     b.HasIndex("UserId");
 
@@ -939,6 +949,16 @@ namespace Intersect.Server.Migrations.Sqlite.Player
                         .HasForeignKey("GuildId")
                         .OnDelete(DeleteBehavior.SetNull);
 
+                    b.HasOne("Intersect.Server.Entities.Player", "PendingGuildInviteFrom")
+                        .WithMany()
+                        .HasForeignKey("PendingGuildInviteFromId")
+                        .OnDelete(DeleteBehavior.SetNull);
+
+                    b.HasOne("Intersect.Server.Database.PlayerData.Players.Guild", "PendingGuildInviteTo")
+                        .WithMany()
+                        .HasForeignKey("PendingGuildInviteToId")
+                        .OnDelete(DeleteBehavior.SetNull);
+
                     b.HasOne("Intersect.Server.Database.PlayerData.User", "User")
                         .WithMany("Players")
                         .HasForeignKey("UserId")
@@ -946,6 +966,10 @@ namespace Intersect.Server.Migrations.Sqlite.Player
                         .IsRequired();
 
                     b.Navigation("Guild");
+
+                    b.Navigation("PendingGuildInviteFrom");
+
+                    b.Navigation("PendingGuildInviteTo");
 
                     b.Navigation("User");
                 });

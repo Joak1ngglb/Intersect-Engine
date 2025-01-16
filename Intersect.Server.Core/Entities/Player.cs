d672b017f7eef19e32f2bedbd9c1787f884e6998
Robbie Lodico <pandacoder@pm.me>
2025-01-16 12:51:35 +0100

    fix: correct bank overflow for items/give #1676 does not appear to happen anymore

--- Intersect.Server.Core/Entities/Player.cs
+++ Intersect.Server.Core/Entities/Player.cs
@@ -2967,7 +2967,7 @@ public partial class Player : Entity
         }
 
         var bankInterface = new BankInterface<BankSlot>(this, Bank);
-        return bankOverflow && bankInterface.TryDepositItem(item, sendUpdate);
+        return bankOverflow && bankInterface.TryDepositItem(item: item, sendUpdate: sendUpdate, giveItem: true);
     }
 
     /// <summary>

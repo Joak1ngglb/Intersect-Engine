668d737f617db232c12d922ff27192c826bb9fe5
BlestroKain <jlrootsloud@gmail.com>
2025-03-24 21:23:46 -0500

    Update Globals.cs

--- Intersect.Client.Core/General/Globals.cs
+++ Intersect.Client.Core/General/Globals.cs
@@ -22,38 +22,6 @@ namespace Intersect.Client.General;
 
 public static partial class Globals
 {
-
-    /// Indica si el otro jugador ha aceptado el intercambio
-    public static bool TradeAccepted { get; set; } = false;
-
-    //Only need 1 table, and that is the one we see at a given moment in time.
-    public static CraftingTableBase ActiveCraftingTable;
-
-    public static int AnimFrame = 0;
-
-    //Bag
-    public static Item[] Bag = null;
-
-    //Bank
-    public static Item[] Bank;
-    public static bool GuildBank;
-    public static int BankSlots;
-
-    public static bool ConnectionLost;
-    // Mail
-    public static List<Mail> Mails = new List<Mail>();
-    /// <summary>
-    /// This is used to prevent the client from showing unnecessary disconnect messages
-    /// </summary>
-    public static bool SoftLogout;
-
-    //Game Systems
-    public static GameContentManager ContentManager;
-
-    public static int CurrentMap = -1;
-
-    public static GameDatabase Database;
-
     //Entities and stuff
     //public static List<Entity> Entities = new List<Entity>();
     public static readonly Dictionary<Guid, Entity> Entities = [];
@@ -263,91 +231,8 @@ public static partial class Globals
         {
             return false;
         }
-    
-    public static Dictionary<Guid, Dictionary<Guid, int>> QuestRewards = new Dictionary<Guid, Dictionary<Guid, int>>();
-    public static Dictionary<Guid, long> QuestExperience = new Dictionary<Guid, long>();
-    public static Dictionary<Guid, Dictionary<JobType, long>> QuestJobExperience = new Dictionary<Guid, Dictionary<JobType, long>>();
 
-    public static List<Guid> GridMaps = new List<Guid>();
-
-    public static bool HasGameData = false;
-
-    public static bool InBag = false;
-
-    public static bool InBank = false;
-
-    //Crafting station
-    public static bool InCraft = false;
-
-    public static bool InShop => GameShop != null;
-
-    public static bool InTrade = false;
-
-    public static bool CanCloseInventory => !(InBag || InBank || InCraft || InShop || InTrade);
-
-    public static GameInput InputManager;
-
-    public static bool IntroComing = true;
-
-    public static long IntroDelay = 2000;
-
-    //Engine Progression
-    public static int IntroIndex = 0;
-
-    public static long IntroStartTime = -1;
-
-    public static bool IsRunning = true;
-
-    public static bool JoiningGame = false;
-
-    public static bool LoggedIn = false;
-
-    //Map/Chunk Array
-    public static Guid[,]? MapGrid;
-
-    public static long MapGridHeight;
-
-    public static long MapGridWidth;
-
-    //Local player information
-    public static Player? Me;
-
-    public static bool MoveRouteActive = false;
-
-    public static bool NeedsMaps = true;
-
-    //Event Guid and the Map its associated with
-    public static Dictionary<Guid, Dictionary<Guid, EventEntityPacket>> PendingEvents =
-        new Dictionary<Guid, Dictionary<Guid, EventEntityPacket>>();
-
-    //Event Show Pictures
-    public static ShowPicturePacket? Picture;
-
-    public static List<Guid> QuestOffers = new List<Guid>();
-    public static Dictionary<Guid, Dictionary<Guid, int>> QuestRewards = new Dictionary<Guid, Dictionary<Guid, int>>();
-    public static Dictionary<Guid, long> QuestExperience = new Dictionary<Guid, long>();
-    public static Dictionary<Guid, Dictionary<JobType, long>> QuestJobExperience = new Dictionary<Guid, Dictionary<JobType, long>>();
-
-
-    public static Random Random = new Random();
-
-    public static GameSystem System;
-
-    //Trading (Only 2 people can trade at once)
-    public static Item[,] Trade;
-
-    //Scene management
-    private static bool _waitingOnServer;
-
-    public static bool WaitingOnServer
-    {
-        get => _waitingOnServer;
-        set => _waitingOnServer = value;
-    }
- 
-    public static Entity GetEntity(Guid id, EntityType type)
-    {
-        if (Entities.ContainsKey(id))
+        if (entity.IsDisposed)
         {
             Entities.Remove(entityId);
             entity = null;
@@ -373,4 +258,10 @@ public static partial class Globals
     }
 
     public static Entity? GetEntity(Guid id, EntityType type) => TryGetEntity(type, id, out var entity) ? entity : null;
+    // Mail
+    public static List<Mail> Mails = new List<Mail>();
+    /// <summary>
+    public static Dictionary<Guid, Dictionary<Guid, int>> QuestRewards = new Dictionary<Guid, Dictionary<Guid, int>>();
+    public static Dictionary<Guid, long> QuestExperience = new Dictionary<Guid, long>();
+    public static Dictionary<Guid, Dictionary<JobType, long>> QuestJobExperience = new Dictionary<Guid, Dictionary<JobType, long>>();
 }
